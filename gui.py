1f74d6d8146219ea761e4bbc1c0819e64c09197f
AlricLobo <39924933+AlricLobo@users.noreply.github.com>
2020-04-21 01:46:34 -0500

    gui has new design with 2 new buttons

--- gui.py
+++ gui.py
@@ -1,22 +1,43 @@
 from tkinter import*
-from data import get_data
+from data import *
+from ranking import *
 #TO-DO make the gui look nicer and more interactive
 #query is the term you search for
 
 root = Tk()
+root.title('Trump Tweet Search')
 
-e = Entry(root,width = 50)
-e.pack()
+search = Entry(root,width = 50)
+search.grid(row = 0, column = 1, padx = 20)
 
+search_label = Label(root, text = "Query")
+search_label.grid(row = 0, column = 0)
+#photo1 = PhotoImage(file = r"C:\Users\alric\Desktop\pa1-spring20\TrumpingTweets\gui_logo.GIF")
+#Label (e,image = photo1, bg = "white").grid(row=0,column = 0, sticky = W)
+
+data = get_data()
+Len = len(data)
 def myClick():
-    query = e.get()
+
+    query = search.get()
+    search.delete(0,END)
     print(query)
-    data = get_data()
-    Len = len(data)
+
+
 
     print(data[19])
-myButton = Button(root, text = "Search", command = myClick)
-myButton.pack()
 
+def nextClick():
+    print("next")
+
+def prevClick():
+    print("prev")
+searchButton = Button(root, text = "Search", command = myClick)
+searchButton.grid(row = 3, column = 0, columnspan = 2, pady = 10, padx = 10, ipadx = 100)
+nextButton = Button(root, text = "Next", command = nextClick)
+nextButton.grid(row = 3, column = 3)
+prevButton = Button(root, text = "Prev", command = prevClick)
+prevButton.grid(row = 3, column = 2)
+#prevButton.pack()
 
 root.mainloop()
